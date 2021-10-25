712fc48bb6442544d83ba9da056b63407dff0dc1
jacovb <vbiljon@gmail.com>
2021-10-25 06:47:59 +0100

    Authentication setup

--- components/AuthPage.js
+++ components/AuthPage.js
@@ -1,8 +1,31 @@
 import { useState, useEffect } from "react";
 import { Auth } from "aws-amplify";
+import "../src/configureAmplify";
+
+const initialState = { email: "", password: "", authCode: "" };
 
 export default function AuthPage() {
-  useEffect(() => {}, []);
+  const [uiState, setUiState] = useState(null);
+  const [formState, setFormState] = useState(initialState);
 
-  return <div></div>;
+  useEffect(() => {
+    checkUser();
+    async function checkUser() {
+      const user = await Auth.currentAuthenticatedUser();
+      console.log({ user });
+    }
+  }, []);
+
+  function onChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <div>
+      <button onClick={() => Auth.federatedSignIn({ provider: "Google" })}>
+        Sign in with Google
+      </button>
+      <button onClick={() => Auth.signOut()}>Sign Out</button>
+    </div>
+  );
 }
