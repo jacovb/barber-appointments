a515dc177bf25d64ec4521c2dac70f4ada91631c
jacovb <vbiljon@gmail.com>
2021-11-19 07:03:46 +0000

    calender past days disabled

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -9,6 +9,9 @@ export default function DatePicker() {
       <Calendar
         onChange={setDate}
         value={date}
+        minDate={new Date()}
+        prev2Label={null}
+        next2Label={null}
         className="bg-neumorph shadow-neumorph px-4 py-4 mx-8 my-8 w-6/12 rounded-lg border-0"
       />
       <p>Selected Date: {date.toDateString()}</p>
