1c42209a5e39b6f5de91716b4ba9d9e6eb506e69
jacovb <vbiljon@gmail.com>
2022-02-22 07:05:31 +0000

    communication with server not there yet

--- components/BookingSummary.js
+++ components/BookingSummary.js
@@ -22,7 +22,7 @@ export default function BookingSummary() {
     fetch("/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: bookingContext.bookingData }),
+      body: JSON.stringify({ items: bookingContext.bookingData }), //get bookingData to server.js. Maybe rename server.js as create-payment-intent and save in api folder
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
